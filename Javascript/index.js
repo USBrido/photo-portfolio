df885c49e4291b7eaf02fc2256dd624bffcbfc2a
Rodrigo <obrido@gmail.com>
2020-11-13 19:00:31 -0500

    Basic version done, with one extra implementation of  gradient mode. Need some refinement to implement Instagram feed live

---
+++ Javascript/index.js
@@ -0,0 +1,34 @@
+const Instafeed = require('https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js');
+
+const navToggle = document.querySelector('.nav-toggle');
+
+const logoToggle = document.querySelector('.logo-toggle');
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+logoToggle.addEventListener('click', () => {
+  document.body.classList.toggle('logo-change');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
+});
+
+const userFeed = new Instafeed({
+  get: 'user',
+  userId: '8987997106',
+  clientId: '924f677fa3854436947ab4372ffa688d',
+  accessToken: '8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9',
+  resolution: 'standard_resolution',
+  template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+  sortBy: 'most-recent',
+  limit: 5,
+  links: false
+});
+userFeed.run();
